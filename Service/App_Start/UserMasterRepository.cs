dec4f7a34ac57265290b2531928ced90502ef0e5
Alkesh kumar <Alkesh kumar@DESKTOP-6LF6M66>
2020-02-24 20:49:27 +0530

    AngularService

---
+++ Service/App_Start/UserMasterRepository.cs
@@ -0,0 +1,28 @@
+﻿using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Service.Models;
+
+namespace Service.App_Start
+{
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        Service.Models.SECURITY_DBEntities context = new SECURITY_DBEntities();
+
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
