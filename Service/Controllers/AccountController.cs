6ec0cb966a9be9c45437451270d5d2d45c7df05f
Alkesh kumar <Alkesh kumar@DESKTOP-6LF6M66>
2020-03-01 23:53:56 +0530

    .

---
+++ Service/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using Service.Models;
+
+namespace Service.Controllers
+{
+    public class AccountController : ApiController
+    {
+        SECURITY_DBEntities db = new SECURITY_DBEntities();
+        [Route("Account/GetUser")]
+        [HttpGet]
+        public IHttpActionResult GetUser()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var Result = db.UserMasters.Where(m => m.UserName == identity.Name);
+            return Ok(Result);
+        }
+    }
+}
